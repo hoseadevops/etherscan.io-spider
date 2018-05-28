9e923fd55a9d4c58f43f03537bdf14217f33800a
hexing <he.xing@9douyu.com>
2018-05-28 14:42:30 +0800

    init

---
+++ spider/spiders/SmartContractSpider.py
@@ -0,0 +1,11 @@
+import scrapy
+
+class SmartContractSpider(scrapy.Spider):
+    name = 'SmartContractSpider'
+    start_urls = ['https://blog.scrapinghub.com']
+    def parse(self, response):
+        for title in response.css('h2.entry-title'):
+            yield {'title': title.css('a ::text').extract_first()}
+
+        for next_page in response.css('div.prev-post > a'):
+            yield response.follow(next_page, self.parse)
